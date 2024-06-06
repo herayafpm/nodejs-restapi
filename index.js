a69069ac8b000738d82caf4ed0be49b53d500272
Heraya Fitra <herayafpm@gmail.com>
2024-06-06 13:23:12 +0700

    add route users get & post

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/',function(req,res){
     });
 })
 
+app.get('/users',function(req,res){
+    res.send({
+        'status' : true,
+        'message': "Berhasil mengambil data users"
+    });
+})
+
+app.post('/users',function(req,res){
+    res.send({
+        'status' : true,
+        'message': "Berhasil mengambil data users dengan method POST"
+    });
+})
+
 var server = app.listen(5000,function(){
     console.log('server listened on http://127.0.0.1:5000')
 })
